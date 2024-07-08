1b3d0b45dec8dda947180526079d20c0f487ee1b
Ritik Raj <iamtheritikraj@gmail.com>
2024-07-08 17:01:44 +0530

    Update UserManagement.cpp

--- UserManagement.cpp
+++ UserManagement.cpp
@@ -1,251 +1,181 @@
 #include <iostream>
-#include <string>
-#include<vector>
-#include<algorithm>
+#include "UserManager.h"
+
 using namespace std;
-class User
-{
-public:
-    string username;
-    string password;
-    string description;
-    bool isloggedIn;
-    User(string uname, string pwd, string desc)
-    {
-        username = uname;
-        password = pwd;
-        description = desc;
-        isloggedIn = false;
-    }
-};
 
-class UserManager
-{
-private:
-    vector<User> users;
-public:
-    void createUser()
-    {
-        string username;
-        string password;
-        string description;
-        cout <<"All fields are mandatory:"<<endl;
-        cout << "Enter Username: ";
-        getline(cin, username);
-        cout << endl;
-        bool pass = false;
-        while (!pass)
-        {
-            cout << "Enter Password: ";
-            getline(cin, password);
-            if (!(password.length() >= 8))
-            {
-                cout << "Password length should be of minimum 8 characters. \n";
-            }
-            else
-            {
-                pass = true;
-            }
 
-        }
-        cout << endl;
-        cout << "Enter Decription: ";
-        getline(cin, description);
-        cout << endl;
-        if (username.empty() || password.empty() || description.empty())
-        {
-            cout << "!! All Fields are required !! \n";
-        }
-        else
-        {
-            users.push_back(User(username, password, description));
-            cout << username + ": User registered successfully!!\n";
-        }
-
-    }
-    void listUsers()
-    {
-        for (const auto& user : users) {
-            cout << "Total created users\n";
-            cout << "Username: " << user.username << ", Description: " << user.description << ", Logged In: " << (user.isloggedIn ? "Yes" : "No") << "\n";
-        }
-    }
-    void modifyDetails()
-    {
-        string username;
-        cout << "Enter username to modify password/decription \n";
-        getline(cin, username);
-        for (auto& user : users) {
-            if (user.username == username&&user.isloggedIn==true) 
-            {
-                int option;
-                cout << "1. Modify password\n";
-                cout << "2. Modify description\n";
-                cout << "3. Exit\n";
-                cout << "Choose Option: ";
-                cin >> option;
-                cin.ignore();
-                cout << endl;
-                switch (option) {
-                case 1 : {
-                    bool pass = false;
-                    while (!pass)
-                    {
-                        cout << "Enter Password: ";
-                        getline(cin, user.password);
-                        if (!(user.password.length() >= 8))
-                        {
-                            cout << "Password length should be of minimum 8 characters. \n";
-                        }
-                        else
-                        {
-                            pass = true;
-                        }
-                    }
-                    cout << "Password modified successfully!\n";
-                    user.isloggedIn = false;
-                    break;
-                }
-                case 2 : {
-                    cout << "Enter new description: ";
-                    getline(cin, user.description);
-                    cout << "Description modified successfully!\n";
-                    user.isloggedIn = false;
-                    break;
-                }
-                case 3:
-                    return;
-                default:
-                    cout << "Invalid option!\n";
-                    break;
-                }
-                return;
-            }
-        }
-        cout << "User not logged in \n";
-        
-    }
-    void loginUser()
-    {
-        bool logged_user = check_loggedStatus();
-        if (!logged_user)
-        {
-            string username;
-            string password;
-            cout << "Enter Username: ";
-            getline(cin, username);
-            for (auto& user : users)
-            {
-                if (username == user.username)
-                {
-                    cout << "Enter Password: ";
-                    getline(cin, password);
-                    if (user.password == password)
-                    {
-                        user.isloggedIn = true;
-                        cout << username + " successfully logged in \n";
-                        
-                    }
-                    else
-                    {
-                        cout << "Wrong password\n";
-                    }
-                    return;
-                }
-            }
-            cout << "Invalid username!!\n";
-        }
-        else
-        {
-            cout << "Another user already logged in\n";
-        }
-        
-    }
-    void logoutUser()
-    {
-        bool logged_user = check_loggedStatus();
-        if (logged_user)
-        {
-            string username;
-            cout << "Enter username: ";
-            getline(cin, username);
-            for (auto& user : users)
-            {
-                if (username == user.username)
-                {
-                    user.isloggedIn = false;
-                    cout << "User successfully logged out\n";
-                    return;
-                }
-            }
-            cout << "Invalid username!!\n";
-        }
-    }
-    bool check_loggedStatus()
-    {
-        for (const auto& user : users) {
-            if (user.isloggedIn) {
-                return true;
-            }
-            
-        }
-        return false;
-    }
-    void loggedInUser()
-    {
-        for (const auto& user : users) {
-            if (user.isloggedIn) {
-                cout << "Logged in user: " << user.username << "\n";
-                return;
-            }
-        }
-        cout << "Currently no users is logged in\n";
-    }
-};
-
-int main()
-{
-    UserManager userManager;
-    cout << "Welcome to the User Management Application" << endl;
-    cout << "Choose Option" << endl;
+int main() {
     int option;
-    while (true) {
-        cout << "\nOptions:\n";
-        cout << "1. Create User\n";
-        cout << "2. List all created Users\n";
-        cout << "3. Modify User\n";
-        cout << "4. Login\n";
-        cout << "5. Logout\n";
-        cout << "6. Display logged in users\n";
-        cout << "7. Exit\n";
+    string username, password, description, newPassword, newDescription;
+
+    while (true)
+    {
+        cout << "\nWelcome to the User Management Application\n";
+        cout << endl;
+        cout << "Choose Option\n";
+        cout << endl;
+        cout << "1) Create User\n";
+        cout << "2) List all created Users\n";
+        cout << "3) Modify User Password\n";
+        cout << "4) Modify User Description\n";
+        cout << "5) Login\n";
+        cout << "6) Logout\n";
+        cout << "7) Display logged in users\n";
+        cout << "8) Delete User\n";
+        cout << "0) Exit\n";
+        cout << endl;
         cout << "Enter your choice: ";
         cin >> option;
-        cin.ignore();
-        cout << endl;
 
         switch (option) {
         case 1:
-            userManager.createUser();
+            cout << "\nEnter username: ";
+            cin >> username;
+            cout << "\nEnter password: ";
+            cin >> password;
+            if (UserManager::Pass_Length(password))
+            {
+                cout << "\nEnter description: ";
+                cin.ignore();
+                getline(cin, description);
+                UserManager::CreateUser(username, password, description);
+            }
+            else
+            {
+                cout << "Password length should be of miniumum 8 characters\n ";
+                cout << endl;
+            }
             break;
-        case 2:
-            userManager.listUsers();
+
+        case 2: {
+            auto users = UserManager::ListAllUsers();
+            cout << "\nList of all users:";
+            cout << endl;
+            for (const auto& user : users) {
+                cout << "Username: " << user.username << ", Description: " << user.description << "";
+            }
             break;
+        }
+
         case 3:
-            userManager.modifyDetails();
+        {
+            cout << "\nEnter username to modify password: ";
+            cin >> username;
+            bool status = UserManager::LoggedInStatus(username);
+            if (status == true)
+            {
+                cout << "\nEnter new password: ";
+                cin >> newPassword;
+                {
+                    if (UserManager::Pass_Length(password))
+                    {
+                        UserManager::ModifyUserPassword(username, newPassword);
+                        UserManager::LogoutUser(username);
+                    }
+                    else
+                    {
+                        cout << "Password length should be of miniumum 8 characters\n ";
+                        cout << endl;
+                    }
+
+                }
+            }
+            else
+            {
+                cout << "\nUser not logged in or Invalid user";
+                cout << endl;
+            }
             break;
+        }
         case 4:
-            userManager.loginUser();
+        {
+            cout << "\nEnter username to modify description: ";
+            cin >> username;
+            bool status = UserManager::LoggedInStatus(username);
+            if (status == true)
+            {
+                cout << "\nEnter new description: ";
+                cin.ignore();
+                getline(cin, newDescription);
+                UserManager::ModifyUserDescription(username, newDescription);
+                UserManager::LogoutUser(username);
+            }
+            else
+            {
+                cout << "\nUser not logged in or Invalid user";
+                cout << endl;
+            }
             break;
+        }
         case 5:
-            userManager.logoutUser();
+        {
+            if (!UserManager::LoggedInStatus())
+            {
+                cout << "\nEnter username: ";
+                cin >> username;
+                cout << "\nEnter password: ";
+                cin >> password;
+                UserManager::LoginUser(username, password);
+            }
+            else
+            {
+                cout << "\nAnother user logged in";
+                cout << endl;
+            }
             break;
+        }
         case 6:
-            userManager.loggedInUser();
+        {
+            if (UserManager::LoggedInStatus())
+            {
+                cout << "\nEnter username to logout: ";
+                cin >> username;
+                UserManager::LogoutUser(username);
+            }
+            else
+            {
+                cout << "\nNo user logged in";
+                cout << endl;
+            }
             break;
-        case 7:
+        }
+
+        case 7: {
+            auto loggedInUsers = UserManager::DisplayLoggedInUsers();
+            {
+                if (loggedInUsers.empty())
+                {
+                    cout << "User is not logged in\n";
+                    cout << endl;
+                }
+                else
+                {
+                    cout << "\nLogged in users: ";
+                    cout << endl;
+                    for (const auto& user : loggedInUsers) {
+                        cout << user << "";
+                    }
+
+                }
+            }
+            break;
+        }
+        case 8:
+        {
+            cout << "\nEnter Username: ";
+            cin >> username;
+            UserManager::DeleteUser(username);
+            break;
+
+        }
+        case 0:
+            cout << "\nExiting...";
             return 0;
+
         default:
-            cout << "Invalid choice! Please try again.\n";
+            cout << "\nInvalid choice......";
+            break;
         }
     }
+
 }
